22c3f0972f20881a20f363a5a83ad5d8d7c76e32
D2WE1 <funnyyin126@126.com>
2017-09-21 19:26:37 +0800

    登录功能和模块化

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,39 @@
-
+define(['jquery','cookie'],function ($) {
 	NProgress.start();
-
 	NProgress.done();
-
+	
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+	
+	//实现退出功能,后台会删除登陆信息的sessionId
+	$('#logoutBtn').click(function () {
+		$.ajax({
+			url: '/api/logout',
+			type: 'post',
+			dataType:'json',
+			success:function (data) {
+				if (data.code==200) {
+					location.href='/main/login'
+				}
+			}
+		})
+	})
+	
+	//返回PHPSESSID 和 loginInfo 两个属性
+	//console.log($.cookie())
+	
+	//检测是否登陆，没登陆信息直接login页面
+	var flag=$.cookie('PHPSESSID')
+	if (!flag) {
+		location.href='/main/login'
+	}
+	
+	//从cookie获取头像信息
+	var loginInfo=$.cookie('loginInfo')
+	loginInfo=loginInfo&&JSON.parse(loginInfo)
+	// console.log(loginInfo)
+	$('.aside .profile img').attr('src',loginInfo.tc_avatar)
+	$('.aside .profile h4').html(loginInfo.tc_name)
+})
+
